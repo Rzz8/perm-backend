dcea057fc477e59b2b870bc02a876f2f37578336
R Zhong <30917072+Rzz8@users.noreply.github.com>
2023-06-14 14:46:36 +1000

    update

--- app.py
+++ app.py
@@ -5,7 +5,33 @@ from pydantic import BaseModel
 import numpy as np
 import pickle
 import pandas as pd
+import tensorflow
+import tensorflow
+from tensorflow.keras.models import Sequential, load_model, save_model, model_from_json
 from fastapi.middleware.cors import CORSMiddleware
+
+# Load model for 3-class classification
+model_json_3c = 'model_clsf_20.0_80.0.json' # .json file with model architecture
+model_weights_3c = 'model_clsf_20.0_80.0.h5' # .h5 file with model weights
+
+# This reads the model architecture and creates the "model" instance
+with open(model_json_3c, 'r') as jsonfile:
+	model_3c = model_from_json(jsonfile.read())
+
+# This loads the model weights
+model_3c.load_weights(model_weights_3c)
+
+# Load model for 2-class classification
+model_json_2c = 'model_clsf_20.0.json' # .json file with model architecture
+model_weights_2c = 'model_clsf_20.0.h5' # .h5 file with model weights
+with open(model_json_2c, 'r') as jsonfile:
+	model_2c = model_from_json(jsonfile.read())
+model_2c.load_weights(model_weights_2c)
+
+# Load regression model 
+pickle_in = open("ann_form_skl.pkl", "rb")
+regressor = pickle.load(pickle_in)
+pickle_in.close()
 
 class PermValue(BaseModel):
     BD: float
@@ -48,10 +74,6 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
-pickle_in = open("ann_form_skl.pkl", "rb")
-regressor = pickle.load(pickle_in)
-pickle_in.close()
-
 mmsx_in = open("minmaxscaler_x.pkl", "rb")
 mmsx = pickle.load(mmsx_in)
 mmsx_in.close()
@@ -86,13 +108,24 @@ def predict_permvalue(data: PermValue):
     X = np.array([[BD, HL, ROP, IMV, DP, DS, GR, RSS, RSD, RSM, PG, FL, FLR, FG]])
     X = add_categorical(X)
     X = mmsx.transform(X)
-    prediction = np.array([regressor.predict(X)])
-    prediction = mmsy.inverse_transform(prediction)[0,0]
-    prediction = round(prediction, 2)
-    text = "The permeability is "
-    unit = "md"
-    prediction = text+ str(prediction) + "  " + unit
-    return {"prediction": prediction}
+    prediction1 = np.argmax(model_3c.predict(X))   # 3-class prediction
+    prediction2 = np.argmax(model_2c.predict(X))   # 2-class prediction
+    prediction3 = np.array([regressor.predict(X)])   # regression prediction  
+    prediction3 = mmsy.inverse_transform(prediction3)[0,0]
+    prediction3 = round(prediction3, 2)  
+    text1 = "The permeability is "
+    text2 = "class "
+    text3 = " (3-classes with 20-80 md cutoff),"
+    text4 = " (2-classes with 20 md cutoff)."
+    unit = "md, "
+    prediction1 = text2+ str(prediction1) + "  " + text3
+    prediction2 = text2+ str(prediction2) + "  " + text4
+    prediction3 = text1+ str(prediction3) + "  " + unit
+    return {
+        "prediction1": prediction1,
+        "prediction2": prediction2,
+        "prediction3": prediction3
+    }
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
